e6994b0a42afc607fd40414195f664256bfae2da
ranel <binijuancruz@gmail.com>
2022-07-28 05:33:33 -0300

    Correcciones en la configuracion de express-handlebar: Solucion a referenceError: __dirname is not defined in es module, y tambien, a exphbs is a not function

--- src/app.js
+++ src/app.js
@@ -3,21 +3,28 @@ import indexRoutes from './routes/index.routes.js'
 import exphbs from 'express-handlebars'
 import path from 'path'
 
+// Solucion a ReferenceError: __dirname is not defined in ES module scope
+/* --------------------------- */
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+/* --------------------------- */
+
+
 const app = express()
 
 //Handlebars
 
 /* Seteamos las views en la carpeta /views */
-app.set('views', path.join(__dirname + '/views'))
+app.set('views', path.join(__dirname , 'views'))
 
-/* Avisamos que en la extension .hbs se encuentra express-handlebars */
-app.engine('.hbs', exphbs({
-
-    /* Definimos la direccion donde se encuentran los layouts */
-    layoutDir: path.join(app.get('views', "layouts")),
+/*Avisamos que en la extension .hbs se encuentra express-handlebars */
+app.set('.hbs', exphbs.engine({
+    // Definimos la direccion donde se encuentran los layouts 
+    layoutsDir: path.join(app.get('views'), "layouts"),
     defaultLayout: "main", // Definimos el archivo main
     extname: ".hbs", // Definimos la extension 
-    
+
 }))
 
 //Routes
